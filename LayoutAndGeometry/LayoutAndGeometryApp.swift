d081e9ad17e1c569611b293d1cddd6b9b7e04036
Takasur Azeem <takasurazeem@gmail.com>
2022-07-30 22:57:07 +0500

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Takasur Azeem on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
