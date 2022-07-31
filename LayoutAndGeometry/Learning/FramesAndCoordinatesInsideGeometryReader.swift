95a1215cafeae9d44c06349d38005df8a8352b31
Takasur Azeem <takasurazeem@gmail.com>
2022-07-31 20:20:39 +0500

    GeometryReader takes up all remaining space
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- LayoutAndGeometry/Learning/FramesAndCoordinatesInsideGeometryReader.swift
+++ LayoutAndGeometry/Learning/FramesAndCoordinatesInsideGeometryReader.swift
@@ -7,9 +7,20 @@
 
 import SwiftUI
 
+
+/// Always keep in mind SwiftUI’s three-step layout system when working with GeometryReader: parent proposes a size for the child, the child uses that to determine its own size, and parent uses that to position the child appropriately.
 struct FramesAndCoordinatesInsideGeometryReader: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            GeometryReader { geo in
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    .frame(width: geo.size.width * 0.9, height: 40)
+                    .background(.green.opacity(0.7))
+            }
+            .background(.green.opacity(0.4))
+            Text("More text")
+                .background(.blue.opacity(0.7))
+        }
     }
 }
 
