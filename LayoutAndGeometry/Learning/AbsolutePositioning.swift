66eb4ea6a764eb57882e7c07c8d01cded43aeaf8
Takasur Azeem <takasurazeem@gmail.com>
2022-07-31 15:27:31 +0500

    Text positioned absolutely (Absolute positioning)
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ LayoutAndGeometry/Learning/AbsolutePositioning.swift
@@ -0,0 +1,21 @@
+//
+//  AbsolutePositioning.swift
+//  LayoutAndGeometry
+//
+//  Created by Takasur Azeem on 31/07/2022.
+//
+
+import SwiftUI
+
+struct AbsolutePositioning: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .position(x: 100, y: 100)
+    }
+}
+
+struct AbsolutePositioning_Previews: PreviewProvider {
+    static var previews: some View {
+        AbsolutePositioning()
+    }
+}
