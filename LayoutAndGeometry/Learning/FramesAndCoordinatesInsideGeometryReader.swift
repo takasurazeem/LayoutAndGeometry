3259886481e570689eb4964b4cf5c38373621a96
Takasur Azeem <takasurazeem@gmail.com>
2022-07-31 16:33:45 +0500

    Understanding frames and coordinates inside GeometryReader
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ LayoutAndGeometry/Learning/FramesAndCoordinatesInsideGeometryReader.swift
@@ -0,0 +1,20 @@
+//
+//  FramesAndCoordinatesInsideGeometryReader.swift
+//  LayoutAndGeometry
+//
+//  Created by Takasur Azeem on 31/07/2022.
+//
+
+import SwiftUI
+
+struct FramesAndCoordinatesInsideGeometryReader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FramesAndCoordinatesInsideGeometryReader_Previews: PreviewProvider {
+    static var previews: some View {
+        FramesAndCoordinatesInsideGeometryReader()
+    }
+}
