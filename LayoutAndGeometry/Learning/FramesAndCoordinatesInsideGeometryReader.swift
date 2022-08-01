7068761e56672019b67b1ff54280690fe29d8e66
Takasur Azeem <takasurazeem@gmail.com>
2022-08-01 23:49:56 +0500

    understand coordinateSpace
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- LayoutAndGeometry/Learning/FramesAndCoordinatesInsideGeometryReader.swift
+++ LayoutAndGeometry/Learning/FramesAndCoordinatesInsideGeometryReader.swift
@@ -11,21 +11,45 @@ import SwiftUI
 /// Always keep in mind SwiftUI’s three-step layout system when working with GeometryReader: parent proposes a size for the child, the child uses that to determine its own size, and parent uses that to position the child appropriately.
 struct FramesAndCoordinatesInsideGeometryReader: View {
     var body: some View {
-        VStack {
-            GeometryReader { geo in
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                    .frame(width: geo.size.width * 0.9, height: 40)
-                    .background(.green.opacity(0.7))
-            }
-            .background(.green.opacity(0.4))
-            Text("More text")
-                .background(.blue.opacity(0.7))
-        }
+        OuterView()
+            .background(.blue.opacity(0.7))
+            .coordinateSpace(name: "Custom")
     }
 }
 
 struct FramesAndCoordinatesInsideGeometryReader_Previews: PreviewProvider {
     static var previews: some View {
         FramesAndCoordinatesInsideGeometryReader()
+    }
+}
+
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
+        }
+    }
+}
+
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { geo in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
     }
 }
