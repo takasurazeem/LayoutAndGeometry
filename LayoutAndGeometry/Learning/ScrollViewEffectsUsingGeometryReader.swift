3a07f263e1346523e17f97f7f1564a8c84e451f2
Takasur Azeem <takasurazeem@gmail.com>
2022-08-04 01:25:03 +0500

    apply a helix-style spinning effect
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ LayoutAndGeometry/Learning/ScrollViewEffectsUsingGeometryReader.swift
@@ -0,0 +1,33 @@
+//
+//  ScrollViewEffectsUsingGeometryReader.swift
+//  LayoutAndGeometry
+//
+//  Created by Takasur Azeem on 03/08/2022.
+//
+
+import SwiftUI
+
+struct ScrollViewEffectsUsingGeometryReader: View {
+    let colors: [Color] = [.green, .blue, .purple, .gray]
+
+    var body: some View {
+        ScrollView {
+            ForEach(0..<50) { index in
+                GeometryReader { geo in
+                    Text("Row #\(index)")
+                        .font(.title)
+                        .frame(maxWidth: .infinity)
+                        .background(colors[index % colors.count])
+                        .rotation3DEffect(.degrees(geo.frame(in: .global).minY / 5), axis: (x: 0, y: 1, z: 0))
+                }
+                .frame(height: 40)
+            }
+        }
+    }
+}
+
+struct ScrollViewEffectsUsingGeometryReader_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewEffectsUsingGeometryReader()
+    }
+}
